401dd69ed7008e10d43d68828be19b6e0227097c
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-23 23:32:41 +0100

    Basic express setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
