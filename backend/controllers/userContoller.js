d6e709e23f14c679f1d835f22998803dfc09ef4f
Abayomi Aishida <yomicorperate@gmail.com>
2022-05-23 20:52:48 +0100

    Login functionality

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const jwt = reqiure("jsonwebtoken");
 
 const User = reqiure("../models/userModel");
 
@@ -39,6 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -50,8 +52,29 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send("Login Route");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  // Check user and passwords match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
 });
+
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
