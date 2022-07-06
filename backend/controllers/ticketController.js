84179cfd2f17ddb6fdc0b9e97f2ea5e6f74a24c9
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-06 14:19:00 +0100

    Get single ticket part 1

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -8,49 +8,78 @@ const Ticket = require("../models/ticketModel");
 // @access Privte
 const getTickets = asyncHandler(async (req, res) => {
   // Get user using the id in the JWT
-  const user = await User.findById(req.user.id)
+  const user = await User.findById(req.user.id);
 
-  if(!user) {
-    res.status(401)
-    throw new Error('User not found')
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
   }
 
-  const tickets= await Ticket.find({user: req.user.id})
+  const tickets = await Ticket.find({ user: req.user.id });
 
   res.status(200).json(tickets);
+});
+
+// @desc   Get user ticket
+// @route  GET /api/tickets/:id
+// @access Privte
+const getTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById({ user: req.params.id });
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  res.status(200).json(ticket);
 });
 
 // @desc   Create new ticket
 // @route  POST /api/tickets
 // @access Privte
 const createTicket = asyncHandler(async (req, res) => {
-  const { product, description } = req.body
+  const { product, description } = req.body;
 
-  if(!product || !description) {
-    res.status(400)
-    throw new Error('Please add a product and description')
+  console.log(req.body);
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("Please add a product and description");
   }
 
   // Get user using the id in the JWT
-  const user = await User.findById(req.user.id)
+  const user = await User.findById(req.user.id);
 
-  if(!user) {
-    res.status(401)
-    throw new Error('User not found')
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
   }
 
   const ticket = await Ticket.create({
     product,
     description,
     user: req.user.id,
-    status: 'new'
-  })
-
+    status: "new",
+  });
 
   res.status(20).json(ticket);
 });
 
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+} = require("../controllers/ticketController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router.route("/:id").get(protect, getTicket);
 
 module.exports = router;
