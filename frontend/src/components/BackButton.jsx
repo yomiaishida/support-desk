fc6090fc1a336fcf9e0e7e2a083d23953d86ee00
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-09 23:18:32 +0100

    Create ticket functionality

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export const BackButton = ({ url }) => {
+  return (
+    <Link to={url} className="btn btn-reverse btn-block">
+      {" "}
+      <FaArrowCircleLeft /> Back
+    </Link>
+  );
+};
