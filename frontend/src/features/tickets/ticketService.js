30c0d4de641699d96ba37c2a269c1c86439d9469
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-22 04:41:23 +0100

    Fetch notes through redux

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -1,63 +1,68 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL  = '/api/tickets/'
+const API_URL = "/api/tickets/";
 
 // Create new ticket
-const createTicket = async(ticketData, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+const createTicket = async (ticketData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const response = await axios.post(API_URL, ticketData, config)
+  const response = await axios.post(API_URL, ticketData, config);
 
-    return response.data
-}
+  return response.data;
+};
 
 // Get user tickets
-const getTickets = async(token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const response = await axios.get(API_URL, config)
+  const response = await axios.get(API_URL, config);
 
-    return response.data
-}
+  return response.data;
+};
 
 // Get user ticket
-const getTicket = async(ticketId, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+const getTicket = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const response = await axios.get(API_URL + ticketId, config)
+  const response = await axios.get(API_URL + ticketId, config);
 
-    return response.data
-}
+  return response.data;
+};
+
 // Close ticket
-const closeTicket = async(ticketId, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+const closeTicket = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const response = await axios.put(API_URL + ticketId, {status: 'closed'}, config)
+  const response = await axios.put(
+    API_URL + ticketId,
+    { status: "closed" },
+    config
+  );
 
-    return response.data
-}
+  return response.data;
+};
 
 const ticketService = {
-    createTicket,
-    getTickets,
-    getTicket,
-    closeTicket
-}
+  createTicket,
+  getTickets,
+  getTicket,
+  closeTicket,
+};
 
-export default ticketService
+export default ticketService;
