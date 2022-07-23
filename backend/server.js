ddf8be00af54f02ee9339f6009dd051396163043
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-23 04:57:15 +0100

    Fixing deploy error

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,9 @@ if (process.env.NODE_ENV === "production") {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
-  );
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("Welcome to the Support Desk API");
