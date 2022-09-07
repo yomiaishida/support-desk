390f6778521afc687e0a4d192dd5fd1be01cc701
Abayomi Aishida <yomicorperate@gmail.com>
2022-09-07 21:48:16 +0100

    FIx: typo error

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -11,11 +11,15 @@ const {
 const { protect } = require("../middleware/authMiddleware");
 
 // Re-route into note router
-const noteRouter = require('./noteRoute')
-router.use('/:ticketId/notes', noteRouter)
+const noteRouter = require("./noteRoutes");
+router.use("/:ticketId/notes", noteRouter);
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
 
-router.route("/:id").get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
